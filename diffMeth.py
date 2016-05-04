0005f48f0e1242c4986a8bc58719023b2a68319f
John <jsh58@unh.edu>
2016-05-04 15:35:50 -0400

    diffMeth: filtering options

--- diffMeth.py
+++ diffMeth.py
@@ -7,15 +7,53 @@ import sys, math
 try:
   from scipy import stats
 except ImportError:
-  print 'Error! Must have the scipy module installed'
+  print 'Error! Must have the scipy module installed --'
+  print '  info can be found at www.scipy.org/install.html'
   sys.exit(-1)
 
 def usage():
-  print "Usage: python diffMeth.py  -1 <sample1List>  -2 <sample2List>  \ \n\
-    -i <input>  -o <output>                                               \n\
-  <sampleList>  Comma-separated list of samples                           \n\
-  <input>       Output from combineRegions2.py                             "
+  print "Usage: python diffMeth.py  -1 <group1>  -2 <group2>  \         \n\
+      -i <input>  -o <output>  [options]                                \n\
+    <group>     Comma-separated list of sample names (as found in       \n\
+                  the header of <input>)                                \n\
+    <input>     File listing genomic regions and methylation results    \n\
+                  (output from combineRegions2.py)                      \n\
+  Options (whether or not to report a region):                          \n\
+    -d <float>  Minimum methylation difference between sample groups    \n\
+                  ([0-1]; def. 0 [all results reported])                \n\
+    -up         Report only regions hypermethylated in group2           \n\
+    -down       Report only regions hypomethylated in group2            \n\
+    -p <float>  Maximum p-value ([0-1]; def. 1 [all results reported])  \n\
+    -dna        Report regions whose diff is 'NA' (occurs when one      \n\
+                  group has no methylation data)                        \n\
+    -pna        Report regions whose p-value is 'NA' (occurs when one   \n\
+                  group does not have multiple data points)              "
   sys.exit(-1)
+
+def openFile(fname):
+  '''
+  Open a file for reading.
+  '''
+  try:
+    f = open(fname, 'rU')
+  except IOError:
+    print 'Error! Cannot open', fname
+    usage()
+  return f
+
+def getFloat(arg):
+  '''
+  Convert given argument to float.
+  '''
+  try:
+    val = float(arg)
+  except ValueError:
+    print 'Error! Cannot convert %s to int' % arg
+    usage()
+  if val > 1 or val < 0:
+    print 'Error! Value %s is not in [0,1]' % arg
+    usage()
+  return val
 
 def getSample(csv):
   '''
@@ -90,7 +128,7 @@ def processLine(line, idx1, idx2, idxExtra):
   avg1, val1, sample1 = calcAvg(spl, idx1)
   avg2, val2, sample2 = calcAvg(spl, idx2)
   if not sample1 and not sample2:
-    return 0, 0, ''
+    return 0, 0, ''  # no data for these groups
   diff = 'NA'
   if sample1 and sample2:
     diff = avg2 - avg1
@@ -116,6 +154,14 @@ def main():
   '''
   Main.
   '''
+  # Default parameters
+  d = 0      # min. methylation difference
+  p = 1      # max. p-value
+  up = 0     # report only hypermethylated (boolean)
+  down = 0   # report only hypomethylated (boolean)
+  dna = 0    # report results with diff of 'NA'
+  pna = 0    # report results with pval of 'NA'
+
   # get command-line args
   args = sys.argv[1:]
   if len(args) < 2: usage()
@@ -126,23 +172,35 @@ def main():
   i = 0
   while i < len(args):
     if args[i] == '-i':
-      try:
-        fIn = open(args[i+1], 'rU')
-      except IOError:
-        print 'Error! Cannot open', args[i+1]
-        usage()
+      fIn = openFile(args[i+1])
     elif args[i] == '-o':
       fOut = open(args[i+1], 'w')
     elif args[i] == '-1':
       sample1 = getSample(args[i+1])
     elif args[i] == '-2':
       sample2 = getSample(args[i+1])
+    elif args[i] == '-d':
+      d = getFloat(args[i+1])
+    elif args[i] == '-p':
+      p = getFloat(args[i+1])
+    elif args[i] == '-up':
+      up = 1
+    elif args[i] == '-down':
+      down = 1
+    elif args[i] == '-dna':
+      dna = 1
+    elif args[i] == '-pna':
+      pna = 1
     elif args[i] == '-h':
       usage()
     else:
       print 'Error! Unknown argument:', args[i]
       usage()
-    i += 2
+
+    if args[i] in ['-up', '-down', '-dna', '-pna']:
+      i += 1
+    else:
+      i += 2
 
   # check for errors
   if fIn is None or fOut is None:
@@ -159,7 +217,16 @@ def main():
   # process file
   for line in fIn:
     diff, pval, res = processLine(line.rstrip(), idx1, idx2, idxExtra)
+
+    # determine if result should be reported
     if res:
+      if diff == 'NA':
+        if not dna: continue
+      elif (up and diff <= 0) or (down and diff >= 0) or \
+          abs(diff) < d: continue
+      if pval == 'NA':
+        if not pna: continue
+      elif pval > p: continue
       fOut.write('\t'.join(res + [str(diff), str(pval)]) + '\n')
 
   fIn.close()
