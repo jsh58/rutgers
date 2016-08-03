1fa78618b9f80eb34cab8410e1411f117bb087dc
John M. Gaspar <jsh58@wildcats.unh.edu>
2016-08-03 17:34:16 -0400

    diffMeth3: p-values

--- diffMeth3.py
+++ diffMeth3.py
@@ -5,27 +5,27 @@
 # Version 2: comparing sets of samples
 
 import sys, math
-#scip = 0  # boolean for scipy import
-#try:
-#  from scipy import stats
-#except ImportError:
-#  print 'Warning! The scipy module is not installed'
-#  print '  (see www.scipy.org/install.html).'
-#  print '  All p-values will be reported as "NA".'
-#  scip = 1
+scip = 0  # boolean for scipy import
+try:
+  from scipy import stats
+except ImportError:
+  sys.stderr.write('Warning! The scipy module is not installed\n'
+    + '  (see www.scipy.org/install.html).\n'
+    + '  All p-values will be reported as "NA".\n')
+  scip = 1
 
 def usage():
-  print "Usage: python diffMeth2.py  [options]  -i <input>  -o <output> \ \n\
-      <groupList>                                                       \n\
-    <groupList> Comma-separated list of sample names (as found in       \n\
-                  the header of <input>)                                \n\
-    <input>     File listing genomic regions and methylation results    \n\
-                  (output from combineRegions2.py)                      \n\
-  Options (whether or not to report a region):                          \n\
-    -c <int>    Minimum number of CpGs in a region (def. 1)             \n\
-    -d <float>  Minimum methylation difference between sample groups    \n\
-                  ([0-1]; def. 0 [all results reported])                 "
-    #-p <float>  Maximum p-value ([0-1]; def. 1 [all results reported])   "
+  print "Usage: python diffMeth3.py  [options]  -i <input>  -o <output> \ \n\
+          <groupList1>  <groupList2>  [...]                               \n\
+    <groupList> Comma-separated list of sample names (as found in         \n\
+                  the header of <input>)                                  \n\
+    <input>     File listing genomic regions and methylation results      \n\
+                  (output from combineRegions2.py)                        \n\
+  Options (whether or not to report a region):                            \n\
+    -c <int>    Minimum number of CpGs in a region (def. 1)               \n\
+    -d <float>  Minimum methylation difference between sample groups      \n\
+                  ([0-1]; def. 0 [all results reported])                  \n\
+    -p <float>  Maximum p-value ([0-1]; def. 1 [all results reported])     "
   sys.exit(-1)
 
 def openFile(fname):
@@ -113,92 +113,107 @@ def saveIndexes(fIn, samples):
   head += resExtra
   for i in range(len(res)-1):
     for j in range(i+1, len(res)):
-      head += ['->'.join([str(i), str(j)])]
+      head += ['->'.join([str(i), str(j)]) + '_diff', \
+        '->'.join([str(i), str(j)]) + '_pval']
   return idxs, idxExtra, head
 
-def calcAvg(spl, idxs):
+def countBases(spl, idxs):
   '''
-  Calculate average for a subset of values in a list.
+  Sum methylated and unmethylated bases from a list.
   '''
-  avg = 0.0
+  meth = unmeth = 0
   sample = 0
   for idx in idxs:
     if spl[idx] == 'NA':
       continue
     div = spl[idx].split('-')
-    meth = getInt(div[0])
-    unmeth = getFloat(div[1])
-    avg += meth / (meth + unmeth)
-    sample += 1
-  if sample:
-    avg /= sample
-  return avg, sample
+    meth += getInt(div[0])
+    unmeth += getInt(div[1])
+  return meth, unmeth
 
-def processLine(line, cpg, idxs, idxExtra):
+def calcDiff(count1, count2):
   '''
-  Calculate mean difference and p-value.
+  Calculate methylation difference and p-value for
+    two samples.
+  '''
+  if count1[0] == 'NA' or count2[0] == 'NA':
+    return 'NA', 'NA'
+
+  # calculate methylation difference
+  n1 = count1[0] + count1[1]
+  n2 = count2[0] + count2[1]
+  p1 = float(count1[0]) / n1
+  p2 = float(count2[0]) / n2
+  diff = p2 - p1
+
+  # calculate Z-value
+  phat = float(count1[0] + count2[0]) / (n1 + n2)
+  if phat in [0.0, 1.0]:
+    zval = 0
+  else:
+    zval = diff / (phat * (1-phat) * (1.0 / n1 + 1.0 / n2))**0.5
+
+  # calculate two-tailed p-value
+  pval = stats.norm.sf(abs(zval))*2
+  return diff, pval
+
+def processLine(line, idxs, idxExtra, minCpG, minDiff, maxPval):
+  '''
+  Process a line containing methylation data for a set of samples.
   '''
   spl = line.split('\t')
   if len(spl) < max([max(idx) for idx in idxs]):
     sys.stderr.write('Error! Poorly formatted record:\n%s' % line)
-  if int(spl[3]) < cpg:
-    return [], 0  # fewer than min. CpGs
+  if int(spl[3]) < minCpG:
+    return []  # fewer than min. CpGs
 
-  # calculate averages
-  avg = []
+  # save counts of meth/unmeth bases
+  count = []
   for idx in idxs:
-    av, sample = calcAvg(spl, idx)
-    if sample:
-      avg.append(av)
+    meth, unmeth = countBases(spl, idx)
+    if meth or unmeth:
+      count.append([meth, unmeth])
     else:
-      avg.append('NA')
+      count.append(['NA'])
 
-  # calculate differences
-  diff = []
-  maxDiff = -1
+  # calculate differences and p-values for each possible
+  #   combination of two samples
+  ans = []  # for saving diffs and p-vals
   pr = 0  # boolean for printing line
-  for i in range(len(avg)-1):
-    for j in range(i+1, len(avg)):
-      if avg[i] == 'NA' or avg[j] == 'NA':
-        diff.append('NA')
-      else:
-        dif = avg[j] - avg[i]
-        diff.append(dif)
-        if abs(dif) > maxDiff:
-          maxDiff = abs(dif)
+  for i in range(len(count)-1):
+    for j in range(i+1, len(count)):
+      diff, pval = calcDiff(count[i], count[j])
+      ans.extend((diff, pval))
+      if diff != 'NA' and abs(diff) >= minDiff \
+          and pval <= maxPval:
         pr = 1
-  if not pr:
-    return [], 0  # no diffs for these groups
 
-  # calculate p-value (Welch's t-test)
-  #if len(sample1) < 2 or len(sample2) < 2 or scip:
-  #  pval = 'NA'
-  #elif diff == 0:
-  #  pval = 1
-  #elif max(sample1) - min(sample1) == 0 and \
-  #    max(sample2) - min(sample2) == 0:
-  #  pval = 0
-  #else:
-  #  pval = stats.ttest_ind(sample1, sample2, equal_var=False)[1]
+  # return if no (significant) diffs for any groups
+  if not pr:
+    return []
 
   # construct record for output file
   res = spl[:4]
-  for av in avg:
-    res.append(str(av))
+  for c in count:
+    if c[0] == 'NA':
+      res.append('NA')
+    else:
+      frac = float(c[0]) / (c[0] + c[1])
+      res.append(str(frac)) # methylation fraction
   for idx in idxExtra:
-    res.append(spl[idx])
-  for dif in diff:
-    res.append(str(dif))
-  return res, maxDiff
+    res.append(spl[idx])  # extra fields
+  for val in ans:
+    res.append(str(val))  # diffs and p-values
+  return res
 
 def main():
   '''
   Main.
   '''
   # Default parameters
-  cpg = 0    # min. number of CpGs
-  d = 0      # min. methylation difference
-  #p = 1      # max. p-value
+  minCpG = 0   # min. number of CpGs
+  minDiff = 0  # min. methylation difference
+  maxPval = 1  # max. p-value
 
   # get command-line args
   args = sys.argv[1:]
@@ -213,11 +228,11 @@ def main():
     elif args[i] == '-o':
       fOut = open(args[i+1], 'w')
     elif args[i] == '-c':
-      cpg = getInt(args[i+1])
+      minCpG = getInt(args[i+1])
     elif args[i] == '-d':
-      d = getFloat(args[i+1], 0, 1)
-    #elif args[i] == '-p':
-      #p = getFloat(args[i+1], 0, 1)
+      minDiff = getFloat(args[i+1], 0, 1)
+    elif args[i] == '-p':
+      maxPval = getFloat(args[i+1], 0, 1)
     elif args[i] == '-h':
       usage()
     else:
@@ -243,11 +258,9 @@ def main():
 
   # process file
   for line in fIn:
-    res, maxDiff = processLine(line.rstrip(), cpg, \
-      idxs, idxExtra)
-
-    # print result
-    if res and maxDiff >= d:
+    res = processLine(line.rstrip(), idxs, idxExtra, \
+      minCpG, minDiff, maxPval)
+    if res:
       fOut.write('\t'.join(res) + '\n')
 
   fIn.close()
