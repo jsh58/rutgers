08b691c1f1357279baad7bace11f864004735a8a
John M. Gaspar <jsh58@wildcats.unh.edu>
2016-10-20 15:35:50 -0400

    getReads: filtering a SAM file

---
+++ getReads.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+# JMG 10/6/16
+# Retrieve subset of reads from a SAM.
+
+import sys
+
+def openRead(filename):
+  '''
+  Open filename for reading. '-' indicates stdin.
+    '.gz' suffix indicates gzip compression.
+  '''
+  if filename == '-':
+    return sys.stdin
+  try:
+    if filename[-3:] == '.gz':
+      f = gzip.open(filename, 'rb')
+    else:
+      f = open(filename, 'rU')
+  except IOError:
+    sys.stderr.write('Error! Cannot open %s for reading\n' % filename)
+    sys.exit(-1)
+  return f
+
+def openWrite(filename):
+  '''
+  Open filename for writing. '-' indicates stdout.
+    '.gz' suffix indicates gzip compression.
+  '''
+  if filename == '-':
+    return sys.stdout
+  try:
+    if filename[-3:] == '.gz':
+      f = gzip.open(filename, 'wb')
+    else:
+      f = open(filename, 'w')
+  except IOError:
+    sys.stderr.write('Error! Cannot open %s for writing\n' % filename)
+    sys.exit(-1)
+  return f
+
+def main():
+  args = sys.argv[1:]
+  if len(args) < 4:
+    print 'Usage: python getReads.py  <inSAM>  (yes|no)  <headers.txt>  <outSAM>'
+    print '  Use \'-\' for stdin/stdout'
+    print '  (yes|no)  Keep reads in headers.txt? \'yes\' or \'no\''
+    sys.exit(-1)
+
+  # load headers -- 1st column of file
+  head = {}
+  fHead = openRead(args[2])
+  for line in fHead:
+    head[line.rstrip().split('\t')[0]] = 1
+  if fHead != sys.stdin:
+    fHead.close()
+
+  # open SAM files
+  fIn = openRead(args[0])
+  fOut = openWrite(args[3])
+  yes = 0
+  if args[1] == 'yes':
+    yes = 1
+
+  # process SAM file
+  if yes:
+    for line in fIn:
+      if line[0] == '@' or line.split('\t')[0] in head:
+        fOut.write(line)
+  else:
+    for line in fIn:
+      if line[0] == '@' or line.split('\t')[0] not in head:
+        fOut.write(line)
+
+  if fIn != sys.stdin:
+    fIn.close()
+  if fOut != sys.stdout:
+    fOut.close()
+
+if __name__ == '__main__':
+  main()
